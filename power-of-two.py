0757c241ca8cdb973387810e4b42293677e21359
vilas <vilaskr2962@gmail.com>
2025-12-18 17:03:15 +0530

    Update Program : [ Vilas ] Power of Two

--- power-of-two.py
+++ power-of-two.py
@@ -2,5 +2,5 @@ num = int(input("Enter number : "))
 if(num <= 0 or num > 31):
     print("Entered Number is out of range")
 else:
-    for i in range(1, num+1):
+    for i in range(num+1):
         print(f"2 ^ {i:<2d} = {2 ** i}")
