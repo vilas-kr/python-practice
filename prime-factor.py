2b6f07a37d69cdcdeb7aa1d2e1f1573fc814c882
vilas <vilaskr2962@gmail.com>
2025-12-18 17:39:18 +0530

    Add Program : [ Vilas ] Prime factor

--- prime-factor.py
+++ prime-factor.py
@@ -1,14 +1,16 @@
 num = int(input("Enter number : "))
-if(num <= 1):
+if(num == 1):
+    print("No prime factor")
+elif(num <= 0):
     print("Invalid input")
-    exit
-i = 2
-while(i*i <= num):
-    if(num%i == 0):
-        print(i, end=", ")
-        num = num//i
-    else:
-        i += 1
-if(num != 0):
-    print(num)    
+else:
+    i = 2
+    while(i*i <= num):
+        if(num%i == 0):
+            print(i, end=", ")
+            num = num//i
+        else:
+            i += 1
+    if(num != 0):
+        print(num)    
     
