531d412bc65877556cd7e3b728d4a1fe56b60009
vilas <vilaskr2962@gmail.com>
2025-12-19 17:47:43 +0530

    Update Program : [ Vilas ] leap year

--- day1/leap_year.py
+++ day1/leap_year.py
@@ -1,6 +1,7 @@
-year = int(input("Enter year : "))
-while(len(str(year)) != 4):
-    year = int(input("Please enter valid year : "))
+year = input("Enter the year : ")
+while(len(year) != 4):
+    year = input("Please enter valid year : ")
+year = int(year)
 if((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
     print(f"{year} is a leap year")
 else:
