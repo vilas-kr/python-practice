6c2ea1da30bf6448fc1492c498f16793064dba91
vilas <vilaskr2962@gmail.com>
2026-01-28 17:57:48 +0530

    Add Program : [ Vilas ] LMS database created

---
+++ database/lms_db/create_lms_db.sql
@@ -0,0 +1,146 @@
+/* 
+The schema represents an LMS with the following entities:
+Users, Courses, Lessons, Enrollments, User Activity, Assessments, and Assessment Submissions.
+
+Relationships include:
+Users enroll in courses
+Courses contain lessons and assessments
+Users perform activities on lessons
+Users submit assessments and receive scores
+*/
+
+CREATE DATABASE learning_management_db;
+GO 
+
+USE learning_management_db;
+GO
+
+CREATE TABLE users (
+	user_id CHAR(8), -- 'USER0001'
+	name VARCHAR(50),
+	email VARCHAR(256),
+	password VARCHAR(50),
+	PRIMARY KEY (user_id)
+);
+GO
+
+ALTER TABLE users
+ADD CONSTRAINT unique_email UNIQUE (email);
+GO 
+
+CREATE TABLE course (
+	course_id CHAR(5), -- 'CU001'
+	course_name VARCHAR(100),
+	course_title VARCHAR(1000),
+	PRIMARY KEY (course_id)
+);
+GO
+
+ALTER TABLE course
+ADD CONSTRAINT unique_course_name UNIQUE (course_name);
+GO
+
+CREATE TABLE lesson (
+	lesson_id CHAR(8), -- 'LES00001'
+	course_id CHAR(5),
+	lesson_title VARCHAR(100),
+	lesson_order SMALLINT,
+	duration TIME(0),
+	PRIMARY KEY (lesson_id)
+);
+GO
+
+ALTER TABLE lesson 
+ADD CONSTRAINT uq_course_lesson_order UNIQUE (course_id, lesson_order)
+GO
+
+ALTER TABLE lesson 
+ADD CONSTRAINT fk_course_lesson FOREIGN KEY (course_id) 
+REFERENCES course (course_id);
+GO
+
+CREATE TABLE enrollment (
+	user_id CHAR(8),
+	course_id CHAR(5),
+	enrollment_date DATETIME2(0),
+	PRIMARY KEY (user_id, course_id)
+);
+GO
+
+ALTER TABLE enrollment 
+ADD CONSTRAINT fk_course_enrollment FOREIGN KEY (course_id) 
+REFERENCES course (course_id);
+GO
+
+ALTER TABLE enrollment 
+ADD CONSTRAINT fk_user_enrollment FOREIGN KEY (user_id) 
+REFERENCES users (user_id);
+GO
+
+CREATE TABLE assessment (
+	assessment_id INT IDENTITY(1, 1),
+	course_id CHAR(5),
+	assessment_name VARCHAR(100),
+	max_score SMALLINT NOT NULL,
+	PRIMARY KEY (assessment_id)
+);
+GO
+
+ALTER TABLE assessment 
+ADD CONSTRAINT fk_course_assessment FOREIGN KEY (course_id) 
+REFERENCES course (course_id);
+GO
+
+CREATE TABLE assessment_submit (
+	assessment_id INT,
+	user_id CHAR(8),
+	obtained_marks INT,
+	submission_date DATETIME2(0),
+	PRIMARY KEY (assessment_id, user_id)
+);
+GO
+
+ALTER TABLE assessment_submit 
+ADD CONSTRAINT fk_user_assessment_submit FOREIGN KEY (user_id) 
+REFERENCES users (user_id);
+GO 
+
+ALTER TABLE assessment_submit 
+ADD CONSTRAINT fk_assessment_assessment_submit FOREIGN KEY (assessment_id) 
+REFERENCES assessment (assessment_id);
+GO 
+
+ALTER TABLE assessment_submit 
+ADD CONSTRAINT df_submission_date DEFAULT GETDATE() FOR submission_date;
+GO
+
+CREATE TABLE user_activity (
+	user_id CHAR(8),
+	lesson_id CHAR(8),
+	activity_status VARCHAR(20),
+	activity_date DATETIME2(0),
+	PRIMARY KEY (user_id, lesson_id)
+);
+GO
+
+ALTER TABLE user_activity
+ADD CONSTRAINT fk_user_user_activity FOREIGN KEY (user_id) 
+REFERENCES users (user_id);
+GO
+
+ALTER TABLE user_activity
+ADD CONSTRAINT fk_lesson_user_activity FOREIGN KEY (lesson_id)
+REFERENCES lesson (lesson_id)
+GO
+
+ALTER TABLE user_activity
+ADD CONSTRAINT ck_status CHECK (activity_status IN ('started', 'completed'));
+GO
+
+ALTER TABLE user_activity 
+ADD CONSTRAINT df_activity_date DEFAULT GETDATE() FOR activity_date;
+GO
+
+
+
+
---
+++ database/lms_db/insert_query.sql
@@ -0,0 +1,112 @@
+-- Insert data to all tables
+USE learning_management_db;
+GO 
+
+INSERT INTO users (user_id, name, email, password) VALUES
+('USER0001', 'Alice', 'alice@gmail.com', 'pass123'),
+('USER0002', 'Bob', 'bob@gmail.com', 'pass456'),
+('USER0003', 'Charlie', 'charlie@gmail.com', 'pass789'),
+('USER0004', 'Diana', 'diana@gmail.com', 'diana123'),
+('USER0005', 'Evan', 'evan@gmail.com', 'pass111'),
+('USER0006', 'Frank', 'frank@gmail.com', 'pass222'),
+('USER0007', 'Vilas', 'vilas@gmail.com', 'vilas123'),
+('USER0008', 'Helen', 'helen@gmail.com', 'pass333'),
+('USER0009', 'Ishan', 'ishan@gmail.com', 'pass444'),
+('USER0010', 'Jenny', 'jenny@gmail.com', NULL);
+GO
+
+INSERT INTO course (course_id, course_name, course_title) VALUES
+('CU001', 'SQL', 'Master SQL from Basics to Advanced'),
+('CU002', 'JAVA', 'Complete Java Programming'),
+('CU003', 'PY', NULL),
+('CU004', 'DSA', 'Data Structures and Algorithms'),
+('CU005', 'WEB', 'Full Stack Web Development');
+GO
+
+INSERT INTO lesson (lesson_id, course_id, lesson_title, lesson_order, duration) VALUES
+('LES00001', 'CU001', 'Introduction to SQL', 1, '00:30:00'),
+('LES00002', 'CU001', 'SQL Joins', 2, '00:45:00'),
+('LES00003', 'CU001', 'Subqueries', 3, '01:00:56'),
+('LES00004', 'CU002', 'Java Basics', 1, '01:00:00'),
+('LES00005', 'CU002', 'OOP Concepts', 2, '00:50:00'),
+('LES00006', 'CU003', 'Python Intro', 1, '01:40:00'),
+('LES00007', 'CU001', 'Indexes & Performance', 4, '00:40:00'),
+('LES00008', 'CU001', 'Stored Procedures', 5, '00:20:00'),
+('LES00009', 'CU002', 'Exception Handling', 3, '00:35:00'),
+('LES00010', 'CU002', 'Collections Framework', 4, '01:10:00'),
+('LES00011', 'CU004', 'DSA Introduction', 1, '00:50:00'),
+('LES00012', 'CU004', 'Arrays & Strings', 2, '01:00:00'),
+('LES00013', 'CU005', 'HTML Basics', 1, '00:45:00'),
+('LES00014', 'CU005', 'CSS Fundamentals', 2, '00:50:00');
+GO
+
+INSERT INTO enrollment (user_id, course_id, enrollment_date) VALUES
+('USER0001', 'CU001', '2025-01-01 10:00:00'),
+('USER0001', 'CU002', '2025-01-05 11:00:00'),
+('USER0002', 'CU001', '2025-02-10 11:30:00'),
+('USER0003', 'CU003', '2025-01-10 09:30:00'),
+('USER0004', 'CU002', '2025-02-15 16:45:00'),
+('USER0005', 'CU001', '2025-02-01 10:15:00'),
+('USER0006', 'CU002', '2025-12-15 16:45:00'),
+('USER0006', 'CU004', '2025-02-05 09:00:00'),
+('USER0007', 'CU005', '2025-02-15 16:34:00'),
+('USER0008', 'CU001', '2025-02-10 11:30:00'),
+('USER0009', 'CU004', '2025-02-12 14:00:00'),
+('USER0010', 'CU005', '2025-02-15 16:45:00');
+GO
+
+INSERT INTO assessment (course_id, assessment_name, max_score) VALUES
+('CU001', 'SQL Basics Test', 100),
+('CU001', 'SQL Advanced Test', 100),
+('CU002', 'Java Fundamentals Test', 50),
+('CU003', 'Python Quiz', 25),
+('CU002', 'Java OOP Test', 100),
+('CU004', 'DSA Basics Test', 50),
+('CU004', 'Arrays Test', 50),
+('CU005', 'HTML & CSS Quiz', 30);
+GO
+
+INSERT INTO assessment_submit (assessment_id, user_id, obtained_marks, submission_date) VALUES
+(1, 'USER0001', 85, '2025-01-15 10:00:00'),
+(1, 'USER0002', 70, '2025-01-27 09:00:00'),
+(2, 'USER0001', 90, '2025-01-20 12:00:00'),
+(3, 'USER0004', NULL, '2025-06-25 10:00:00'),
+(4, 'USER0003', 20, '2025-01-25 09:00:00'),
+(5, 'USER0001', 78, '2025-02-03 10:00:00'),
+(5, 'USER0006', NULL, '2025-01-25 09:00:00'),
+(6, 'USER0006', 45, '2025-02-07 09:30:00'),
+(7, 'USER0009', 40, '2025-01-25 09:10:00'),
+(8, 'USER0010', 25, '2025-02-16 17:00:00');
+GO
+
+INSERT INTO user_activity (user_id, lesson_id, activity_status, activity_date) VALUES
+('USER0001', 'LES00001', 'started', '2025-01-02 10:00:00'),
+('USER0001', 'LES00002', 'started', '2025-01-02 08:30:00'),
+('USER0002', 'LES00001', 'started', '2025-01-03 09:00:00'),
+('USER0003', 'LES00006', 'completed', '2025-02-02 10:30:00'),
+('USER0004', 'LES00004', 'started', '2025-01-06 08:30:00'),
+('USER0005', 'LES00001', 'started', '2025-02-02 09:00:00'),
+('USER0005', 'LES00002', 'completed', '2025-02-02 10:30:00'),
+('USER0006', 'LES00004', 'started', '2025-05-11 15:00:00'),
+('USER0006', 'LES00009', 'completed', '2025-02-06 11:00:00'),
+('USER0007', 'LES00013', 'started', '2025-02-08 12:00:00'),
+('USER0007', 'LES00014', 'completed', '2025-12-08 12:00:00'),
+('USER0008', 'LES00003', 'started', '2025-02-11 15:00:00'),
+('USER0009', 'LES00011', 'completed', '2025-02-16 19:00:00'),
+('USER0010', 'LES00013', 'started', '2025-02-16 16:00:00');
+GO 
+
+select * from users;
+
+select * from course;
+
+select * from lesson;
+
+select * from enrollment;
+
+select * from assessment;
+
+select * from assessment_submit;
+
+select * from user_activity;
+
